8c56fac90d0efc31eccf43716b74e40973761495
clug1 <simba328@cau.ac.kr>
2017-09-28 14:30:31 +0900

    first

---
+++ first.c
@@ -0,0 +1,27 @@
+/*
+program use
+
+complie 방법
+>> gcc example.c
+
+
+콘솔창 프로그램 실행
+>> /a.out name
+
+output
+
+안녕하세요 name 님
+
+클러그 부스에 오신것을 환영합니다.
+이번 클러그 부스에서는 git을 활용한 행사를 진행하고 있습니다.
+
+이 소스코드는 18년 전 영국에서 부터 만들어져 리눅스를 만든
+linus torvalds와 Alan Mathison Turing를 거쳐 clug 부스까지
+오게 되었으며 여러분은 그 역사 앞에 위치하고 있습니다.
+
+여러분이 이 유구한 역사 속에서 여러분 자신을 찾을 수 있으셨으면 합니다.
+
+* 본 행사는 linus torvalds와 Alan Mathison Turing와 무관함을 알려드립니다.
+
+*/
+
